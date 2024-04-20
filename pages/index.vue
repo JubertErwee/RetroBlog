51eb776aa9970cecebf5e497e1727557b17f4df3
Kazo0o <kizito.thibile@gmail.com>
2024-04-20 11:08:50 +0200

    Added bootstrap package

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,27 @@
+
 <script setup lang="ts">
+
 const version = 2 + 1
 </script>
 
 <template>
-  <div class="hello">
-    Hello Nuxt {{ version }}!
-  </div>
-  <li><nuxt-link to="/about" class="btn">About</nuxt-link></li>
+  <div class="container">
+  <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+    <p>For more information visit website</p>
+    <b-button variant="primary" href="#">More Info</b-button>
+  </b-jumbotron>
+     
+<div>
+  <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+</div>
+</div>
 </template>
 
 <style scoped>
