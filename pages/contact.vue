93a08585fb1746c898de0dbe851c8d9bbf4efaf9
Paballo - A Software Developer and Data Scientist <113717207+Paballo2002@users.noreply.github.com>
2024-04-20 10:42:34 +0200

    kaunch json added on project run

---
+++ pages/contact.vue
@@ -0,0 +1,8 @@
+<template>
+    <form>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message"></textarea>
+      <button type="submit">Send Message</button>
+    </form>
+</template>
