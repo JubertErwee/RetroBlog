edc1e3a392f089de7131aa58a777636bafc486e4
JubertErwee <167604329+JubertErwee@users.noreply.github.com>
2024-04-20 11:44:55 +0200

    Boostrap on cantact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+  <!-- <div class="container">
     <div class="row">
       <div class="col-md-6 offset-md-3">
         <h2 class="text-center mb-4">Contact Us</h2>
@@ -20,10 +20,27 @@
         </form>
       </div>
     </div>
+  </div> -->
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h2 class="text-center mb-4">Contact Us</h2>
+        <form>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Enter your name">
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+          </div>
+          <div class="form-group">
+            <label for="message">Message</label>
+            <textarea class="form-control" id="message" rows="5" placeholder="Enter your message"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Submit</button>
+        </form>
+      </div>
+    </div>
   </div>
-  <!-- Bootstrap JS and dependencies -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </template>
-
